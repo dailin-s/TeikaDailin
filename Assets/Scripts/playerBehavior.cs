27134bb438929b22bfe9e66558118d05425f9019
dailin <dailinsiggers@gmail.com>
2026-01-26 20:13:52 -0600

    collision & movement
    was able to add collision to borders and all sprites. ran into an issue with writing the first movement script.

---
+++ Assets/Scripts/playerBehavior.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class playerBehavior : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update() 
+    {
+        float update = 0.0f;
+        if (Keyboard.current.leftArrowKey.wasPressedThisFrame) {
+            update = -speed;
+        }
+        Vector3 newPosition = transform.position;
+        newPosition.x = transform.position.x + update;
+        transform.position = newPosition;
+    }
+}
