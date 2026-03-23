115d651eed379df40bc65b1033a36fe9bc7810f9
dailin <dailinsiggers@gmail.com>
2026-03-23 00:29:18 -0500

    added main menu
    the button doesnt work properly

---
+++ Assets/Scripts/MenuBehavior.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class MenuBehavior : MonoBehaviour
+{
+    public void gotoGame() {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Main");
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
